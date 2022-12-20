58b331e688d9e56cd5a6ef21cbf2c011d4f278f2
Lecislav <lecislav@protonmail.com>
2022-12-20 18:15:51 +0100

    click and restart

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 'use strict';
-console.log(document.querySelector("body"));
+//variables>>>
+const user= {
+    fields : [],
+    
+};
+
+const computer = {
+    fields: [],
+};
+//variables<<<
+
+//core>>>
+assignEventToTableElements();
+assignEventToRestartButton();
+//core<<<
+
+
+//functions>>
+function assignEventToTableElements(){
+    for (let i = 1; i < 10; i++) {
+     console.log(document.getElementById(i.toString()));
+    document.getElementById(i.toString()).addEventListener('click',clickOnTableElement);
+    }
+}
+function clickOnTableElement(ele){
+    this.textContent = "X";
+    console.log(this);
+}
+function assignEventToRestartButton(){
+    document.querySelector(".buttonRestart").addEventListener('click',()=>{
+       for (let i = 1; i < 10; i++) {
+        document.getElementById(i.toString()).textContent="";
+         console.log('lol');
+       }
+    });
+}
