d18306b15fab15f8a4c257ebd51c285761b41b84
Lecislav <lecislav@protonmail.com>
2022-12-24 00:06:11 +0100

    add win func and modals

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict';
 //variables>>>
 const table= new Map();
-
+let modals;
+const userMark = "X";
+const computerMark = "O"
 //variables<<<
 
 //core>>>
 cleaningTable();
 assignEventToTableElements();
 assignEventToRestartButton();
+assignEventToModalWindow();
 //core<<<
 
 
@@ -20,12 +23,18 @@ function assignEventToTableElements(){
     }
 }
 function clickOnTableElement(ele){
-    if (this.textContent != "X" || this.textContent != "O") {
-      this.textContent = "X";
+    if (this.textContent != userMark || this.textContent != computerMark) {
+      this.textContent = userMark;
     }
     console.log(this.textContent)
     this.removeEventListener('click',clickOnTableElement);
-    if(isEmptyField())computerMove();
+   ;
+    if (!checkForWinner(userMark) && isEmptyField()) {
+      computerMove();
+      checkForWinner(computerMark);
+    } else {
+
+    }
 }
 function assignEventToRestartButton(){
     document.querySelector(".buttonRestart").addEventListener('click',cleaningTable);
@@ -40,7 +49,7 @@ function cleaningTable(){
 function computerMove(){
     let number = drawPosition();
     console.log(number);
-    while ( number == 0 || table.get(number).textContent == "O" ||table.get(number).textContent == "X") {
+    while ( number == 0 || table.get(number).textContent == computerMark ||table.get(number).textContent == userMark) {
       number = drawPosition();
       console.log(number);
     }
@@ -49,10 +58,44 @@ function computerMove(){
 }
 function isEmptyField(){
     for(const ele of table.values()){
-        if (ele.textContent == "") return true;
+        if (ele.textContent != computerMark && ele.textContent != userMark) return true;
     }
    return false;
 }
 function drawPosition(){
     return Math.round(Math.random()*9);
+}
+function checkForWinner(mark){
+    const wynik = new Array();
+    for(const ele of table.values()){
+        if(ele.textContent == mark)wynik.push(ele.id);
+    }
+    if ((wynik.includes("1") && wynik.includes("2")&&wynik.includes('3')) ||
+        (wynik.includes("4") && wynik.includes("5")&&wynik.includes('6')) ||
+        (wynik.includes("7") && wynik.includes("8")&&wynik.includes('9')) ||
+        (wynik.includes("1") && wynik.includes("4")&&wynik.includes('7')) ||
+        (wynik.includes("2") && wynik.includes("5")&&wynik.includes('8')) ||
+        (wynik.includes("3") && wynik.includes("6")&&wynik.includes('9')) ||
+        (wynik.includes("1") && wynik.includes("5")&&wynik.includes('9')) ||
+        (wynik.includes("3") && wynik.includes("5")&&wynik.includes('7')) ) {
+
+      console.log(`wygrywa ${mark}`);
+      console.log(wynik);
+      for (const ele of modals) {
+        ele.style.display = "block";
+      }
+    }
+    return false;
+}
+function assignEventToModalWindow(){
+    modals = document.querySelectorAll(".hidden");
+    console.log(modals);
+    for(const ele of modals){
+        ele.addEventListener('click',()=>{
+            for(const element of modals){
+            element.style.display ='none';
+            }
+        })
+        console.log(ele);
+    }
 }
